646b47b2b9de56c4272531f5583ce66966c93e8b
hmy0223 <42632474+hmy0223@users.noreply.github.com>
2021-01-19 23:04:14 +0800

    setup project

---
+++ weatherApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  weatherApp
+//
+//  Created by Ho Man Yee on 19/1/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
